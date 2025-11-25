fa484604da704f424851e3d814e3f3f953c792c5
Ramil <zarram89@gmail.com>
2025-11-25 10:36:52 +0500

    feat: adds page state switch

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { createAds } from './data.js';
 import { renderCard } from './card.js';
+import { disablePage, enablePage } from './form.js';
 
 const mapCanvas = document.querySelector('#map-canvas');
 const ads = createAds();
@@ -7,3 +8,6 @@ const ads = createAds();
 if (ads.length > 0) {
   mapCanvas.appendChild(renderCard(ads[0]));
 }
+
+disablePage();
+enablePage();
