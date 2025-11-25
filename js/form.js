7b935441573790b1983cffea5c842efeade079ba
Ramil <zarram89@gmail.com>
2025-11-25 11:15:10 +0500

    feat: adds validation

--- js/form.js
+++ js/form.js
@@ -4,6 +4,63 @@ const mapFilters = document.querySelector('.map__filters');
 const mapFiltersSelects = mapFilters.querySelectorAll('select');
 const mapFiltersFieldsets = mapFilters.querySelectorAll('fieldset');
 
+const roomNumberField = adForm.querySelector('#room_number');
+const capacityField = adForm.querySelector('#capacity');
+
+const roomsToGuests = {
+  1: ['1'],
+  2: ['1', '2'],
+  3: ['1', '2', '3'],
+  100: ['0']
+};
+
+const guestsToRooms = {
+  0: ['100'],
+  1: ['1', '2', '3'],
+  2: ['2', '3'],
+  3: ['3']
+};
+
+const pristine = new Pristine(adForm, {
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__element--invalid',
+  successClass: 'ad-form__element--valid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'ad-form__error'
+});
+
+const validateCapacity = () => roomsToGuests[roomNumberField.value].includes(capacityField.value);
+
+const getCapacityErrorMessage = () => {
+  const rooms = roomNumberField.value;
+  if (rooms === '100') {
+    return 'Не для гостей';
+  }
+  return `Для ${rooms} комнат(ы) можно выбрать не более ${rooms} гостей`;
+};
+
+pristine.addValidator(capacityField, validateCapacity, getCapacityErrorMessage);
+pristine.addValidator(roomNumberField, validateCapacity, getCapacityErrorMessage);
+
+const onRoomNumberChange = () => {
+  pristine.validate(capacityField);
+};
+
+const onCapacityChange = () => {
+  pristine.validate(roomNumberField);
+};
+
+roomNumberField.addEventListener('change', onRoomNumberChange);
+capacityField.addEventListener('change', onCapacityChange);
+
+adForm.addEventListener('submit', (evt) => {
+  const isValid = pristine.validate();
+  if (!isValid) {
+    evt.preventDefault();
+  }
+});
+
 /**
  * Переводит страницу в неактивное состояние.
  */
