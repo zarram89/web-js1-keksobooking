1127ddb1d0ca5ceb998f4342990c629020d8eb60
Ramil <zarram89@gmail.com>
2025-11-25 10:19:40 +0500

    feat: adds card generation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 import { createAds } from './data.js';
+import { renderCard } from './card.js';
 
-// eslint-disable-next-line no-console
-console.log(createAds());
+const mapCanvas = document.querySelector('#map-canvas');
+const ads = createAds();
+
+if (ads.length > 0) {
+  mapCanvas.appendChild(renderCard(ads[0]));
+}
